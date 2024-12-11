69b1de03e8c60790f93d71028e3988633a4cc439
vishnu10082005 <vishnu.preetham@kalvium.community>
2024-12-11 17:04:22 +0530

    Errors Fixed in the Main branch

--- Backend Server/Config Files/DB.js
+++ Backend Server/Config Files/DB.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
-const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const connection = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected`);
   } catch (error) {
